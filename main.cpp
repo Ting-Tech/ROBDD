06698a054aa7894fca4a9e7e9ebfcade3f87211f
allenting <allen0523win@gmail.com>
2023-09-14 17:55:26 +0800

    finish initialize sheet and fixing logic.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main()
     ifstream inputFile;
     ofstream outputFile;
 
+    inputFile.open("test.txt");
+
     string inputStr;
 
     int i = 0, o = 0, p = 0;
@@ -19,10 +21,14 @@ int main()
     vector<char> ilb;
     vector<vector<char>> sheet;
     vector<string> logicSheet;
+    vector<vector<char>> trueCombination;
 
     while (true)
     {
         string command;
+
+        inputFile >> command;
+        cout << command << endl;
 
         if (command == ".i")
         {
@@ -50,9 +56,12 @@ int main()
 
         else if (command == ".ilb")
         {
-            char sign;
-            inputFile >> sign;
-            ilb.push_back(sign);
+            for (size_t c = 0; c < i; c++)
+            {
+                char sign;
+                inputFile >> sign;
+                ilb.push_back(sign);
+            }
         }
 
         else if (command == ".ob")
@@ -61,8 +70,7 @@ int main()
         else if (command == ".p")
         {
             int num = 0;
-            vector<vector<char>> trueCombination;
-            inputFile >> p >> num;
+            inputFile >> num;
 
             for (size_t a = 0; a < num; a++) // 讀True的組合
             {
@@ -76,50 +84,92 @@ int main()
                 trueCombination.push_back(stateCombination);
             }
             // 製作出表格陣列
-            for (size_t j = 0, signCount = 0, edgeCount = 2; j < p; j++)
+            for (size_t j = 0, signCount = 0, edgeCount = 2; j < pow(2, i) - 1; j++)
             {
-                if (j > pow(2, signCount)) // 判斷是否下一層了
-                    signCount++;
-                for (size_t k = 0; k < 3; k++) // 計算表格數值
+                if (j > (pow(2, signCount) - 2)) // 判斷是否下一層了
                 {
-                    if (i == 0) // 第一row數值
-                    {
-                        sheet.push_back({'0', 'X', 'X', '0'});
-                    }
-                    else if (i == (pow(2, i) - 1)) // 最後一row數值
-                    {
-                        sheet.push_back({'1', 'X', 'X', '1'});
-                    }
-                    else // 中間row的數值
-                    {
-                        char variable, elseEdge, thenEdge, comment;
+                    signCount++;
+                }
+                // 計算表格數值
+                // if (j == 0) // 第一row數值
+                // {
+                //     sheet.push_back({'0', 'X', 'X', '0'});
+                // }
+                // else if (j == (pow(2, i) - 1)) // 最後一row數值
+                // {
+                //     sheet.push_back({'1', 'X', 'X', '1'});
+                // }
+                // else // 中間row的數值
+                // {
+                char variable, elseEdge, thenEdge, comment;
 
-                        variable = (97 + signCount); // 符號判斷
-                        if (signCount == i)
+                variable = (96 + signCount); // 符號判斷
+                if (signCount == i)
+                {
+                    for (size_t c = 0; c < pow(2, i); c++) // 結果判斷
+                    {
+                        for (size_t d = 0; d < trueCombination.size(); d++)
                         {
-                            for (size_t c = 1; c < pow(2, i); c++) // 結果判斷
+                            for (size_t e = 0; e < i; e++)
                             {
+                                if ((trueCombination[d][e] != logicSheet[c][e]) && trueCombination[d][e] != '-')
+                                {
+                                    if (c % 2 == 0)
+                                    {
+                                        elseEdge = '0';
+                                    }
+                                    else
+                                    {
+                                        thenEdge = '0';
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    if (c % 2 == 0)
+                                    {
+                                        elseEdge = '1';
+                                    }
+                                    else
+                                    {
+                                        thenEdge = '1';
+                                    }
+                                }
                             }
                         }
-                        else
-                        {
-                            elseEdge = edgeCount;
-                            edgeCount++;
-                            thenEdge = edgeCount;
-                            edgeCount++;
-                        }
-                        comment = 'X';
-                        sheet.push_back({variable, elseEdge, thenEdge, comment});
                     }
+                    comment = 'X';
+                    sheet.push_back({variable, elseEdge, thenEdge, comment});
                 }
+                else
+                {
+                    elseEdge = edgeCount + 48;
+                    edgeCount++;
+                    thenEdge = edgeCount + 48;
+                    edgeCount++;
+                    comment = 'X';
+                    sheet.push_back({variable, elseEdge, thenEdge, comment});
+                }
+                // }
             }
         }
 
         else if (command == ".e")
         {
-            for (auto &logic : logicSheet)
+            // for (auto &logic : logicSheet)
+            // {
+            //     cout << logic << endl;
+            // }
+            // cout << sheet.size() << endl;
+            // cout << trueCombination.size() << endl;
+            for (auto &sheets : sheet)
             {
-                cout << logic << endl;
+                for (auto &element : sheets)
+                {
+                    cout << element;
+                }
+                // cout << sheets[0];
+                cout << endl;
             }
             return 0;
         }
